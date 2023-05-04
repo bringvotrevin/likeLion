ee25a859ebd1416e3e18d7b0a624b93a96d0d44b
dasom im <dim@student.42seoul.kr>
2023-05-04 22:52:23 +0900

    Add: 0503 React

---
+++ React/0504/my-app/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import TripList from './components/tripList/TripList';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+function App() {
+
+  return (
+    <div>
+      <TripList />
+    </div>
+  );
+}
+
+export default App;
