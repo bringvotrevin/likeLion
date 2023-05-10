475528d8721ed374bae343dacf5ff3f046cba0b0
dasom im <dim@student.42seoul.kr>
2023-05-10 23:16:08 +0900

    Add: 0510 React

---
+++ React/0510/compomentlist/src/components/Counter.jsx
@@ -0,0 +1,19 @@
+import { React, useState } from 'react';
+
+export default function Counter() {
+  const [number, setNumber] = useState(0);
+  function increment() {
+    setNumber((prevNumber) => prevNumber + 1);
+  }
+  function decrement() {
+    setNumber((prevNumber) => prevNumber - 1);
+    // setNumber(number - 1);
+  }
+  return (
+    <div>
+      <h2>Counter : {number}</h2>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
+    </div>
+  );
+}
