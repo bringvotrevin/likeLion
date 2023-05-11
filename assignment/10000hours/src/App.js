5e6457a2f21e58a244cc0358b9af18b4bf1accb5
dasom im <dim@student.42seoul.kr>
2023-05-11 23:31:03 +0900

    Add: 0511 React 1만시간의 법칙 실습

---
+++ assignment/10000hours/src/App.js
@@ -0,0 +1,12 @@
+// import 
+
+function App() {
+  return (
+    <div className='wrapper'>
+      <div className='title'>
+        <img src='./img/title.png' alt='타이틀' />
+      </div>
+    </div>
+  );
+}
+export default App;
