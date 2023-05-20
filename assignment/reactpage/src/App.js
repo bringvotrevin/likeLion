ac776e16890be1db5d086aed939c292af56e6228
dasom im <bringvotrevin@gmail.com>
2023-05-20 17:27:31 +0900

    Feat: assignment/10000hours, reactpage

--- assignment/reactpage/src/App.js
+++ assignment/reactpage/src/App.js
@@ -1,15 +1,29 @@
+import { BrowserRouter, Routes, Route, Link, useLocation } from "react-router-dom";
 import GameList from "./components/GameList";
 import Header from "./components/Header/Header";
 import Description from "./components/Description";
 
 
 function App() {
+  // return (
+  //   <div>
+  //     <Header/>
+  //     <GameList/>
+  //     <Description/>
+  //   </div>
+  // );
+
   return (
-    <div>
-      <Header/>
-      <GameList/>
-      <Description/>
-    </div>
-  );
+    <BrowserRouter >
+      <Link to='/game'>game</Link>
+      <Link to='/header'>header</Link>
+      <Link to='/description'>description</Link>
+      <Routes>
+        <Route path='/' element={<GameList />}/>
+        <Route path='/header' element={<Header />}/>
+        <Route path='/description' element={<Description />}/>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 export default App;
